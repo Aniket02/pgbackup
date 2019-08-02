d63711f2c6abb03bd52cac78bc666a831d14f973
Aniket Shah <aniket02shah@gmail.com>
2019-08-02 00:41:03 -0400

    Implement Google Cloud Strorage upload

--- src/pgbackup/storage.py
+++ src/pgbackup/storage.py
@@ -2,3 +2,9 @@ def local(infile, outfile):
     outfile.write(infile.read())
     outfile.close()
     infile.close()
+
+
+def google_storage(storage_client, source_name, bucket_name, dest_name):
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(dest_name)
+    blob.upload_from_filename(source_name)
